e6649e250f4a52c10e396bca31fb574ee39562a8
phoenix21212 <80974944+phoenix21212@users.noreply.github.com>
2023-09-11 21:15:16 +0200

    initial commit

---
+++ lua/plugins/notify.lua
@@ -0,0 +1,22 @@
+return{{
+   'rcarriga/nvim-notify',
+   config = function()
+      vim.notify = require('notify').setup({
+         stages = "slide",
+         timeout = 2500,
+         fps = 60,
+         position = "bottom_right",
+         -- render = "compact"
+         icons = {
+            ERROR = "",
+            WARN = "",
+            INFO = "",
+            DEBUG = "",
+            TRACE = "✎",
+         },
+         render = "wrapped-compact",
+         top_down = false,
+
+      })
+   end
+}}
