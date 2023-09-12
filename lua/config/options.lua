4f34b5e01d40b04e1ac38a182eca5c4ef4f31957
phoenix21212 <80974944+phoenix21212@users.noreply.github.com>
2023-09-12 14:00:43 +0200

    Test 2

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,11 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.opt.guicursor = {
+   "n-v:block",
+   "i-c-ci-ve:ver25",
+   "r-cr:hor20",
+   "o:hor50",
+   "i:blinkwait100-blinkoff400-blinkon400-Cursor/lCursor",
+   "sm:block-blinkwait175-blinkoff150-blinkon175",
+}
