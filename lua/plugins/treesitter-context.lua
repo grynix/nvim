60c8e030a359f46ea3ca6fd8c4de3686881664fd
phoenix21212 <80974944+phoenix21212@users.noreply.github.com>
2023-09-24 16:51:59 +0200

    change b close 2

--- lua/plugins/treesitter-context.lua
+++ lua/plugins/treesitter-context.lua
@@ -1,26 +1,23 @@
 -- Include the Treesitter context setup directly within the plugin specification
 return {
    -- Your other plugins and configurations...
- 
    -- Add and configure nvim-treesitter-context
    {
-     "nvim-treesitter/nvim-treesitter-context",
-     config = function()
-       require'treesitter-context'.setup{
-         enable = true,
-         max_lines = 0,
-         min_window_height = 0,
-         line_numbers = true,
-         multiline_threshold = 20,
-         trim_scope = 'outer',
-         mode = 'cursor',
-         separator = nil,
-         zindex = 20,
-         on_attach = nil,
-       }
-     end,
+      "nvim-treesitter/nvim-treesitter-context",
+      config = function()
+         require("treesitter-context").setup({
+            enable = true,
+            max_lines = 0,
+            min_window_height = 0,
+            line_numbers = true,
+            multiline_threshold = 20,
+            trim_scope = "outer",
+            mode = "cursor",
+            separator = nil,
+            zindex = 20,
+            on_attach = nil,
+         })
+      end,
    },
- 
    -- Your other plugins and configurations...
- }
- 
+}
