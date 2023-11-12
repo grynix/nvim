5b0acdb6bf514ef463913008b488004646ab167c
phoenix21212 <80974944+phoenix21212@users.noreply.github.com>
2023-11-12 20:59:48 +0100

    FEAT + cmd paste change
    sq
    
    Fix

--- init.lua
+++ init.lua
@@ -1,19 +1,11 @@
 vim.keymap.set("v", "<D-c>", '"+y') -- Copy
-vim.keymap.set("n", "<D-v>", '"+P') -- Paste normal mode
-vim.keymap.set("v", "<D-v>", '"+P') -- Paste visual mode
+vim.keymap.set("n", "<D-v>", '"+p') -- Paste normal mode
+vim.keymap.set("v", "<D-v>", '"+p') -- Paste visual mode
 vim.keymap.set("c", "<D-v>", "<C-R>+") -- Paste command mode
-vim.keymap.set("i", "<D-v>", '<ESC>l"+Pli') -- Paste insert mode
+vim.keymap.set("i", "<D-v>", '<C-o>"+p') -- Paste insert mode
 
 if vim.g.neovide then
 	vim.g.neovide_cursor_animation_length = 0
 end
 
 require("config.lazy")
-
--- vim.opt.tabstop = 3
--- vim.opt.softtabstop = 3
--- vim.opt.shiftwidth = 3
--- vim.opt.expandtab = false
--- vim.opt.autoindent = true
--- vim.opt.smartindent = true
--- vim.opt.clipboard = ""
