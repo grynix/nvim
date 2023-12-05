cc06fea529ce7fb5044e24bef22fe6690299688e
phoenix21212 <80974944+phoenix21212@users.noreply.github.com>
2023-12-05 21:05:40 +0100

    Colorscheme improvements

--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -49,20 +49,21 @@ return {
 			color_overrides = {
 				mocha = {
 					rosewater = "#efc9c2",
-					flamingo = "#ebb2b2",
-					pink = "#f2a7de",
+					flamingo = "#BB9AF7",
+					pink = "#BA99F5",
 					mauve = "#b889f4",
-					red = "#f52f2f",
-					maroon = "#ea838c",
-					peach = "#f39967",
+					red = "#DF6C83",
+					maroon = "#E0AF68", -- This is opts in a fn
+					peach = "#FF9E64",
 					yellow = "#f5c462",
 					green = "#96d382",
-					teal = "#78cec1",
+					-- teal = "#0DB9D7", -- we'll need this in the future
+					teal = "#1ABC9C",
 					sky = "#91d7e3",
 					sapphire = "#68bae0",
 					blue = "#739df2",
-					lavender = "#b1b8fa",
-					text = "#b5c1f1",
+					lavender = "#73DACA",
+					text = "#C0CAF5",
 					subtext1 = "#a6b0d8",
 					subtext0 = "#959ec2",
 					overlay2 = "#848cad",
@@ -72,12 +73,53 @@ return {
 					surface1 = "#3e4255",
 					surface0 = "#2c2f40",
 					base = "#1a1c2a",
-					mantle = "#141620",
+					baseMantle = "#181924",
+					mantle = "#16161E",
 					crust = "#0e0f16",
 				},
 			},
 			custom_highlights = function(colors)
-				return { FlashLabel = { fg = "white", bg = "#f53100" } }
+				local active_bg = O.transparent_background and colors.none or colors.baseMantle
+				local inactive_bg = O.transparent_background and colors.none or colors.baseMantle
+				return {
+					FlashLabel = {
+						fg = "white",
+						bg = "#FF007C",
+					},
+					-- NeoTree
+					NeoTreeDirectoryName = { fg = colors.blue },
+					NeoTreeDirectoryIcon = { fg = colors.blue },
+					NeoTreeNormal = { fg = colors.text, bg = active_bg },
+					NeoTreeNormalNC = { fg = colors.text, bg = active_bg },
+					NeoTreeExpander = { fg = colors.overlay0 },
+					NeoTreeIndentMarker = { fg = colors.overlay0 },
+					NeoTreeRootName = { fg = colors.blue, style = { "bold" } },
+					NeoTreeSymbolicLinkTarget = { fg = colors.pink },
+
+					NeoTreeGitAdded = { fg = colors.green },
+					NeoTreeGitConflict = { fg = colors.red },
+					NeoTreeGitDeleted = { fg = colors.red },
+					NeoTreeGitIgnored = { fg = colors.overlay0 },
+					NeoTreeGitModified = { fg = colors.yellow },
+					NeoTreeGitUnstaged = { fg = colors.red },
+					NeoTreeGitUntracked = { fg = colors.mauve },
+					NeoTreeGitStaged = { fg = colors.green },
+
+					NeoTreeFloatBorder = { link = "FloatBorder" },
+					NeoTreeFloatTitle = { link = "FloatTitle" },
+					NeoTreeTitleBar = { fg = colors.mantle, bg = colors.blue },
+
+					NeoTreeFileNameOpened = { fg = colors.pink },
+					NeoTreeDimText = { fg = colors.overlay1 },
+					NeoTreeFilterTerm = { fg = colors.green, style = { "bold" } },
+					NeoTreeTabActive = { bg = active_bg, fg = colors.lavender, style = { "bold" } },
+					NeoTreeTabInactive = { bg = inactive_bg, fg = colors.overlay0 },
+					NeoTreeTabSeparatorActive = { fg = active_bg, bg = active_bg },
+					NeoTreeTabSeparatorInactive = { fg = inactive_bg, bg = inactive_bg },
+					NeoTreeVertSplit = { fg = colors.base, bg = inactive_bg },
+					NeoTreeStatusLineNC = { fg = colors.mantle, bg = colors.mantle },
+					--
+				}
 			end,
 		},
 	},
