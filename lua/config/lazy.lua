e51ef2c67108d9cc1f7774a79b07f03859dbea42
phoenix21212 <80974944+phoenix21212@users.noreply.github.com>
2023-12-05 19:59:07 +0100

    ColorScheme

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -28,21 +28,25 @@ require("lazy").setup({
 		version = false, -- always use the latest git commit
 		-- version = "*", -- try installing the latest stable version for plugins that support semver
 	},
-	install = { colorscheme = { "habamax", "tokyonight" } },
-	checker = { enabled = true }, -- automatically check for plugin updates
-	performance = {
-		rtp = {
-			-- disable some rtp plugins
-			disabled_plugins = {
-				"gzip",
-				-- "matchit",
-				-- "matchparen",
-				-- "netrwPlugin",
-				"tarPlugin",
-				"tohtml",
-				"tutor",
-				"zipPlugin",
-			},
+	install = {
+		missing = true,
+		colorscheme = { "tokyonight" },
+	},
+	rtp = {
+		-- disable some rtp plugins
+		disabled_plugins = {
+			"gzip",
+			-- "matchit",
+			-- "matchparen",
+			-- "netrwPlugin",
+			"tarPlugin",
+			"tohtml",
+			"tutor",
+			"zipPlugin",
 		},
+	},
+	checker = {
+		enabled = true,
+		notify = false,
 	},
 })
