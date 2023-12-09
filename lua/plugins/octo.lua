0ac9a7905d4090726e3923220c9d270803dffee9
phoenix21212 <80974944+phoenix21212@users.noreply.github.com>
2023-12-09 16:21:26 +0100

    feature - octo

---
+++ lua/plugins/octo.lua
@@ -0,0 +1,11 @@
+return {
+	"pwntester/octo.nvim",
+	requires = {
+		"nvim-lua/plenary.nvim",
+		"nvim-telescope/telescope.nvim",
+		"nvim-tree/nvim-web-devicons",
+	},
+	opts = function()
+		require("octo").setup()
+	end,
+}
