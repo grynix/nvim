30058d3f7f4a66805b96b1a7a3bb4b2a3f82fd2e
phoenix21212 <80974944+phoenix21212@users.noreply.github.com>
2024-01-07 18:14:25 +0100

    Turn off scroll animation

--- init.lua
+++ init.lua
@@ -5,6 +5,7 @@ vim.keymap.set("c", "<D-v>", "<C-R>+") -- Paste command mode
 vim.keymap.set("i", "<D-v>", '<C-o>"+P') -- Paste insert mode
 
 if vim.g.neovide then
+	vim.g.neovide_scroll_animation_length = 0
 	vim.g.neovide_cursor_animation_length = 0
 end
 
