743c95a440380df495c640f595a7fda7a6d68844
phoenix21212 <80974944+phoenix21212@users.noreply.github.com>
2024-03-14 18:39:48 +0100

    feat tmux config, mapping to Meta key

--- init.lua
+++ init.lua
@@ -1,10 +1,9 @@
-vim.keymap.set("v", "<D-c>", '"+y') -- Copy
-vim.keymap.set("n", "<D-v>", '"+p') -- Paste normal mode
-vim.keymap.set("v", "<D-v>", '"+p') -- Paste visual mode
-vim.keymap.set("c", "<D-v>", "<C-R>+") -- Paste command mode
-vim.keymap.set("i", "<D-v>", '<C-o>"+P') -- Paste insert mode
-
 if vim.g.neovide then
+	vim.keymap.set("v", "<D-c>", '"+y') -- Copy
+	vim.keymap.set("n", "<D-v>", '"+p') -- Paste normal mode
+	vim.keymap.set("v", "<D-v>", '"+p') -- Paste visual mode
+	vim.keymap.set("c", "<D-v>", "<C-R>+") -- Paste command mode
+	vim.keymap.set("i", "<D-v>", '<C-o>"+P') -- Paste insert mode
 	vim.g.neovide_scroll_animation_length = 0
 	vim.g.neovide_cursor_animation_length = 0
 end
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -16,48 +16,48 @@ _G.conform_format_on_leave = function()
 	-- the {} will break, do not change it to that
 	require("conform").format({ async = true, lsp_fallback = true })
 end
-
 vim.keymap.set("n", "<F8>", "<cmd>AerialNavToggle<CR>")
+
 --  vim.keymap.set('n', '<leader>cz', format, { noremap = true, silent = true })
 --  vim.cmd [[ autocmd InsertLeave * lua on_insert_leave() ]]
 
 -- vim.api.nvim_set_keymap('i', '<esc>', format, { noremap = true, silent = true })
 -- vim.api.nvim_set_keymap('i', '<esc>', '<cmd>lua format_and_return_to_normal()<CR>', { noremap = true, silent = true })
 
-map("n", "<D-s>", "<cmd>w<cr>", { desc = "Save file" })
-map("i", "<D-s>", "<cmd>w<cr>", { desc = "Save file" })
+map("n", "<M-s>", "<cmd>w<cr>", { desc = "Save file" })
+map("i", "<M-s>", "<cmd>w<cr>", { desc = "Save file" })
 
 -- window size change
-map("n", "<D-Up>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
-map("n", "<D-Down>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
-map("n", "<D-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
-map("n", "<D-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
+map("n", "<M-Up>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
+map("n", "<M-Down>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
+map("n", "<M-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
+map("n", "<M-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
 
 map("n", "gb", "%", { desc = "Go to bracket. Acts as %" })
 
 -- windows
 map("n", "<leader>wb", "<C-W>p", { desc = "Other window", remap = true })
 map("n", "<leader>ww", "<C-W>c", { desc = "Delete window", remap = true })
-map("n", "<D-x>", "<C-W>c", { desc = "Delete window", remap = true })
-map("n", "<D-b>", "<C-W>h<C-W>c", { desc = "Close left window", remap = true })
+map("n", "<M-x>", "<C-W>c", { desc = "Delete window", remap = true })
+map("n", "<M-b>", "<C-W>h<C-W>c", { desc = "Close left window", remap = true })
 map("n", "<leader>w`", "<C-W>s", { desc = "Split window below", remap = true })
 map("n", "<leader>w\\", "<C-W>v", { desc = "Split window right", remap = true })
 map("n", "<leader>`", "<C-W>s", { desc = "Split window below", remap = true })
 map("n", "<leader>\\", "<C-W>v", { desc = "Split window right", remap = true })
 
-map("n", "<M-Down>", "<cmd>m .+1<CR>==", { desc = "Shift row Down." })
-map("i", "<M-Down>", "<cmd>m .+1<CR><esc>==i", { desc = "Shift row Down." })
-map("n", "<M-Up>", "<cmd>m .-2<CR>==", { desc = "Shift row Up." })
-map("i", "<M-Up>", "<cmd>m .-2<CR><esc>==i", { desc = "Shift row Up." })
+map("n", "<A-Down>", "<cmd>m .+1<CR>==", { desc = "Shift row Down." })
+map("i", "<A-Down>", "<cmd>m .+1<CR><esc>==i", { desc = "Shift row Down." })
+map("n", "<A-Up>", "<cmd>m .-2<CR>==", { desc = "Shift row Up." })
+map("i", "<A-Up>", "<cmd>m .-2<CR><esc>==i", { desc = "Shift row Up." })
 
 -- Git
 map("n", "<leader>gg", "<cmd>Neogit<CR>", { desc = "Neogit" })
 map("n", "<leader>git", "<cmd>LazyGit<CR>", { desc = "LazyGit" })
 map("n", "<leader>gdt", "<cmd>diffthis<CR>", { desc = "Diff this", remap = true })
 map("n", "<leader>gf", "<cmd>Neotree float git_status<CR>", { desc = "Neotree: git_status" })
-map("n", "<D-g>", "<cmd>Telescope git_status initial_mode=normal<CR>", { desc = "Telescope: git_status", remap = true })
-map("n", "<D-b>", "<cmd>Git blame_line<CR>", { desc = "Toggle Current Line Blame" })
-map("n", "<D-B>", "<cmd>Git toggle_current_line_blame<CR>", { desc = "Toggle Current Line Blame" })
+map("n", "<M-g>", "<cmd>Telescope git_status initial_mode=normal<CR>", { desc = "Telescope: git_status", remap = true })
+map("n", "<M-b>", "<cmd>Git blame_line<CR>", { desc = "Toggle Current Line Blame" })
+map("n", "<M-B>", "<cmd>Git toggle_current_line_blame<CR>", { desc = "Toggle Current Line Blame" })
 map(
 	"n",
 	"<leader>gdc",
@@ -75,17 +75,17 @@ map("n", "<leader>ghf", "<cmd>DiffviewFileHistory %<CR>", { desc = "Open file hi
 map("n", "<leader>ghh", "<cmd>DiffviewFileHistory<CR>", { desc = "Open file history for project" })
 
 -- CMD keys
-map("n", "<D-j>", "<cmd>exe 'normal! o'<CR>", { desc = "Empty line below" })
-map("i", "<D-j>", "<C-o>o", { desc = "Empty line below" })
+map("n", "<M-j>", "<cmd>exe 'normal! o'<CR>", { desc = "Empty line below" })
+map("i", "<M-j>", "<C-o>o", { desc = "Empty line below" })
 
-map("n", "<D-k>", "<cmd>exe 'normal! O'<CR>", { desc = "Empty line above" })
-map("i", "<D-k>", "<cmd>exe 'normal! O'<CR>", { desc = "Empty line above" })
+map("n", "<M-k>", "<cmd>exe 'normal! O'<CR>", { desc = "Empty line above" })
+map("i", "<M-k>", "<cmd>exe 'normal! O'<CR>", { desc = "Empty line above" })
 
-map("n", "<D-o>", "yyp", { desc = "Duplicate current line" })
-map("i", "<D-o>", "<ESC>yypA", { desc = "Duplicate current line" })
-map("v", "<D-o>", "d2P", { desc = "Duplicate current line" })
+map("n", "<M-o>", "yyp", { desc = "Duplicate current line" })
+map("i", "<M-o>", "<ESC>yypA", { desc = "Duplicate current line" })
+map("v", "<M-o>", "d2P", { desc = "Duplicate current line" })
 
-map("n", "<D-p>", "a<CR><CR><UP><C-o>p<ESC><cmd>LazyFormat<CR>", { desc = "Paste after in new line" })
+map("n", "<M-p>", "a<CR><CR><UP><C-o>p<ESC><cmd>LazyFormat<CR>", { desc = "Paste after in new line" })
 
 map("v", "<C-p>", "p", { desc = "Paste and change the register for what we replaced" })
 map("v", "p", '"_xP', { desc = "Paste without changing the register" })
@@ -93,32 +93,32 @@ map("v", "p", '"_xP', { desc = "Paste without changing the register" })
 map("n", "J", "i<CR><CR><UP><TAB><C-o>", { desc = "enter" })
 
 -- 'a' registry
-map("n", "<D-t>", 'v"aiwp', { desc = "Paste the current word from 'a' registry" })
-map("n", "<D-y>", '"ayiw', { desc = "Yank word into 'a' registry" })
-map("v", "<D-y>", '"ay', { desc = "Yank into 'a' registry" })
+map("n", "<M-t>", 'v"aiwp', { desc = "Paste the current word from 'a' registry" })
+map("n", "<M-y>", '"ayiw', { desc = "Yank word into 'a' registry" })
+map("v", "<M-y>", '"ay', { desc = "Yank into 'a' registry" })
 
 map("n", "x", '"_x', { desc = "x into 'b' buffer" })
 map("n", "X", "viwp", { desc = "Replace the current word with content from regular registry" })
 map("v", "x", '"_x', { desc = "x into 'b' buffer" })
 
 -- Spectre / word search
-map("n", "<D-f>g", function()
+map("n", "<M-f>g", function()
 	require("spectre").open_visual({ select_word = true })
 end, { desc = "Search current word globally" })
 
-map("n", "<D-f>l", function()
+map("n", "<M-f>l", function()
 	require("spectre").open_file_search({ select_word = true })
 end, { desc = "Search current word in current file" })
 
-map("n", "<D-f>d", function()
+map("n", "<M-f>d", function()
 	require("spectre").toggle()
 end, { desc = "Toggle word search" })
 
-map("n", "<D-f>b", "<cmd>Telescope current_buffer_fuzzy_find<CR>", { desc = "Telescope in current buffer" })
+map("n", "<M-f>b", "<cmd>Telescope current_buffer_fuzzy_find<CR>", { desc = "Telescope in current buffer" })
 
-map("v", "<D-f>", ":'<,'>lua require('spectre').open_visual()<CR>", { desc = "Search selected word globally" })
+map("v", "<M-f>", ":'<,'>lua require('spectre').open_visual()<CR>", { desc = "Search selected word globally" })
 
-map("n", "<D-d>", "*N//", { desc = "Search for word", remap = true })
+map("n", "<M-d>", "*N//", { desc = "Search for word", remap = true })
 map("n", "<leader>sb", "<cmd>Telescope buffers initial_mode=normal<CR>", { desc = "Search in buffers" })
 
 map("n", "<F4>", "<cmd>LazyFormat<CR>", { desc = "Format document" })
