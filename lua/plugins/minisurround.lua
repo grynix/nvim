576e78a511d3a8db86351c71a33c8ea2ec87a902
Mark Hegedus <mark@qualisync.com>
2025-01-07 19:33:21 +0100

    update minisurr hotkeys

--- lua/plugins/minisurround.lua
+++ lua/plugins/minisurround.lua
@@ -3,12 +3,12 @@ return {
 	opts = {
 		mappings = {
 			add = "s", -- Add surrounding in Normal and Visual modes
-			delete = "Sd", -- Delete surrounding
-			find = "Sf", -- Find surrounding (to the right)
-			find_left = "SF", -- Find surrounding (to the left)
-			highlight = "Sh", -- Highlight surrounding
-			replace = "Sr", -- Replace surrounding
-			update_n_lines = "Sn", -- Update `n_lines`
+			delete = "S", -- Delete surrounding
+			replace = "<c-s>", -- Replace surrounding
+			-- find = "Sf", -- Find surrounding (to the right)
+			-- find_left = "SF", -- Find surrounding (to the left)
+			-- highlight = "Sh", -- Highlight surrounding
+			-- update_n_lines = "Sn", -- Update `n_lines`
 		},
 	},
 }
