a03b0a14d2d76cc1958d9403bd824c9ddc3fa384
Mark Hegedus <mark@qualisync.com>
2025-02-07 23:42:56 +0100

    fix indent.mini
    s

--- lua/plugins/indent.lua
+++ lua/plugins/indent.lua
@@ -1,39 +1,38 @@
 return {
-	{ -- Options which control scope computation
-		"echasnovski/mini.indentscope",
-		version = false, -- wait till new 0.7.0 release to put it back on semver
-		event = "LazyFile",
-		opts = {
-			draw = {
-				-- Delay (in ms) between event and start of drawing scope indicator
-				delay = 0,
+	"echasnovski/mini.indentscope",
+	version = "*",
+	opts = {
+		draw = {
+			-- Delay (in ms) between event and start of drawing scope indicator
+			delay = 0,
 
-				-- Animation rule for scope's first drawing. A function which, given
-				-- next and total step numbers, returns wait time (in ms). See
-				-- |MiniIndentscope.gen_animation| for builtin options. To disable
-				animation = require("mini.indentscope").gen_animation.linear({
-					easing = "in",
-					duration = 8,
-					unit = "step",
-				}),
+			-- Animation rule for scope's first drawing. A function which, given
+			-- next and total step numbers, returns wait time (in ms). See
+			-- |MiniIndentscope.gen_animation| for builtin options. To disable
 
-				-- Symbol priority. Increase to display on top of more symbols.
-				priority = 2,
-			},
-			options = {
-				-- Type of scope's border: which line(s) with smaller indent to
-				-- categorize as border. Can be one of: 'both', 'top', 'bottom', 'none'.
-				border = "both",
+			animation = function(next, total)
+				-- if next > total / 2 then
+				-- 	return 0
+				-- end
+				return 8
+			end,
 
-				-- Whether to use cursor column when computing reference indent.
-				-- Useful to see incremental scopes with horizontal cursor movements.
-				indent_at_cursor = true,
+			-- Symbol priority. Increase to display on top of more symbols.
+			priority = 2,
+		},
+		options = {
+			-- Type of scope's border: which line(s) with smaller indent to
+			-- categorize as border. Can be one of: 'both', 'top', 'bottom', 'none'.
+			border = "both",
 
-				-- Whether to first check input line to be a border of adjacent scope.
-				-- Use it if you want to place cursor on function header to get scope of
-				-- its body.
-				try_as_border = true,
-			},
+			-- Whether to use cursor column when computing reference indent.
+			-- Useful to see incremental scopes with horizontal cursor movements.
+			indent_at_cursor = true,
+
+			-- Whether to first check input line to be a border of adjacent scope.
+			-- Use it if you want to place cursor on function header to get scope of
+			-- its body.
+			try_as_border = true,
 		},
 	},
 }
